bbfaa62a9329b4d70657b01d5acc4e1fa3d4484a
Berkay Dede <berkayyy.555@gmail.com>
2021-08-05 22:23:57 +0300

    clear button works

--- resources/script.js
+++ resources/script.js
@@ -1,26 +1,29 @@
 addButton = document.getElementById("addToDo");
 groceryList = document.getElementById("groceryList");
-inputText = document.getElementById("inputText")
-
+inputText = document.getElementById("inputText");
+clearToDo = document.getElementById("clearToDo");
 //adding new items to the list, first one define the function, second one with clicking add button on screen
 //third one is with enter key
 
 function addItem() {
     if (inputText.value === ""){
         alert("You must enter some text")
-    } else{
-    listItem = document.createElement("ul");
-    listItem.classList.add("listItem-styling");
-    //paragraph.setAttribute("id")
-    groceryList.appendChild(listItem);
-    listItem.innerHTML = inputText.value;
-    inputText.value = ""}
+    } else {
+        listItem = document.createElement("li");
+        listItem.classList.add("listItem-styling");
+        // listItem.setAttribute("id", inputText.value.toLowerCase());
+        //paragraph.setAttribute("id")
+        groceryList.appendChild(listItem);
+        listItem.innerHTML = inputText.value;
+        inputText.value = ""
+    }
 }
 addButton.addEventListener("click", function () {
     addItem()
 })
 
 inputText.addEventListener("keypress", (event) => {
+    // when enter pressed
     if (event.which === 13) {
         addItem();
     }
@@ -28,20 +31,24 @@ inputText.addEventListener("keypress", (event) => {
 
 
 
+
 //making list item line through
 groceryList.addEventListener("click", e => {
-    if(e.target.tagName == "UL"){
-        e.target.classList.toggle("checked")
-    }
+    console.log("item pressed.");
+    console.log(e);
+    e.path[0].classList.toggle("checked")
+    
 })
 
-/*
+
+
 //delete list item when click Clear
-removeTask.addEventListener('click', function(e) {
-    if(e.target.tagName == "UL"){
-    node.parentNode.removeChild("UL");}
+clearToDo.addEventListener('click', function() {
+    console.log("clear btn pressed");
+    // remove all child of grovceryList
+    groceryList.innerHTML = '';
 });
-*/
+
 
 
 
